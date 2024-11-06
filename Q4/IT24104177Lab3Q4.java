85cf5d0c41aa68cdfe29c64e02c36e065534b98e
anudha23 <anudhaumavin0@gmail.com>
2024-11-06 13:25:03 +0530

    IT24104177Lab3Q4.java

--- Q4/IT24104177Lab3Q4.java
+++ Q4/IT24104177Lab3Q4.java
@@ -6,9 +6,9 @@ public class IT24104177Lab3Q4{
         System.out.print("Enter a five-digit number: ");
         int number = scanner.nextInt();
         
-        // Ensure the input is a five-digit number
+       
         if (number >= 10000 && number <= 99999) {
-            // Separate and print digits
+            
             String numStr = String.valueOf(number);
             for (int i = 0; i < numStr.length(); i++) {
                 System.out.print(numStr.charAt(i) + " ");
